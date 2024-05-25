b9f3e69f09dad8064a72f5625549701dce5882e7
michaldrozd <michaldrozd03@gmail.com>
2024-05-25 17:26:30 +0200

    Added deleting task

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import {ChangeEvent, FC, useState} from "react";
 import {ITask} from "./interfaces.ts";
+import TodoTask from "./Components/TodoTask.tsx";
 
 const App: FC = () => {
     const [task, setTask] = useState<string>("");
     const [deadline, setDeadline] = useState<number>(0);
-    const [todoList, setToDoList] = useState<ITask[]>([]);
+    const [todoList, setTodoList] = useState<ITask[]>([]);
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
         if (event.target.name === "task") {
@@ -17,11 +18,17 @@ const App: FC = () => {
 
     const addTask = (): void => {
         const newTask = {taskName: task, deadline: deadline};
-        setToDoList([...todoList, newTask]);
+        setTodoList([...todoList, newTask]);
         setTask("");
         setDeadline(0);
         console.log(todoList);
     }
+
+    const completeTask = (taskNameToDelete: string): void => {
+        setTodoList(todoList.filter((task) => {
+            return task.taskName !== taskNameToDelete;
+        }))
+    };
 
     return (
         <>
@@ -33,9 +40,13 @@ const App: FC = () => {
                 </div>
                 <button onClick={addTask}>Add Task</button>
             </div>
-            <div className="todoList"></div>
+            <div className="todoList">
+                {todoList.map((task: ITask, key: number) => {
+                    return <TodoTask key={key} task={task} completeTask={completeTask}/>;
+                })}
+            </div>
         </>
     )
 }
 
-export default App
+export default App;
