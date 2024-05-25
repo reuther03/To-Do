1070d9a9b6fb250e44cfa64ee7499d5fd95fa872
michaldrozd <michaldrozd03@gmail.com>
2024-05-25 03:32:02 +0200

    Added component for adding new task

---
+++ src/components/TodoWrapper.tsx
@@ -0,0 +1,19 @@
+﻿import React, {useState} from 'react';
+import TodoForm from "./TodoForm.tsx";
+import {uuid} from "uuidv4";
+const TodoWrapper = () => {
+    const [todos, setTodos] = useState()
+
+    const addTodo = (todo) => {
+        setTodos([...todos, { id: uuid(), task: todo, completed: false, isEditing: false }])
+        console.log(todos)
+    }
+
+    return (
+        <div className="TodoWrapper">
+            <TodoForm addTodo={addTodo}/>
+        </div>
+    );
+};
+
+export default TodoWrapper;
